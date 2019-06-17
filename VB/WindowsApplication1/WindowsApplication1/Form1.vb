7fb3ee7db9869254db955afd3da41be236595b89
DevExpressExampleBot <fakeEmail>
2019-06-17 10:33:56 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -11,11 +10,12 @@ Imports DevExpress.XtraCharts
 Namespace WindowsApplication1
 	Partial Public Class Form1
 		Inherits Form
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 			' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
 			Me.productsTableAdapter.Fill(Me.nwindDataSet.Products)
 
