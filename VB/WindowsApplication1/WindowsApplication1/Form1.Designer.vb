4af63912d7cd6032ce2844514e9811b027cc1d9c
DevExpressExampleBot <fakeEmail>
2023-12-08 11:02:15 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -1,96 +1,96 @@
-﻿Namespace WindowsApplication1
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WindowsApplication1
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
-			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-			Me.nwindDataSet = New WindowsApplication1.nwindDataSet()
-			Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.productsTableAdapter = New WindowsApplication1.nwindDataSetTableAdapters.ProductsTableAdapter()
-			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' chartControl1
-			' 
-			Me.chartControl1.DataAdapter = Me.productsTableAdapter
-			Me.chartControl1.DataSource = Me.productsBindingSource
-			xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
-			xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-			Me.chartControl1.Diagram = xyDiagram1
-			Me.chartControl1.Location = New System.Drawing.Point(24, 16)
-			Me.chartControl1.Name = "chartControl1"
-			Me.chartControl1.SeriesDataMember = "SupplierID"
-			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
-			Me.chartControl1.SeriesTemplate.ArgumentDataMember = "CategoryID"
-			Me.chartControl1.SeriesTemplate.SummaryFunction = "SUM([UnitsInStock])"
-			Me.chartControl1.Size = New System.Drawing.Size(592, 432)
-			Me.chartControl1.TabIndex = 0
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.chartControl1.BoundDataChanged += new DevExpress.XtraCharts.BoundDataChangedEventHandler(this.chartControl1_BoundDataChanged);
-			' 
-			' nwindDataSet
-			' 
-			Me.nwindDataSet.DataSetName = "nwindDataSet"
-			Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' productsBindingSource
-			' 
-			Me.productsBindingSource.DataMember = "Products"
-			Me.productsBindingSource.DataSource = Me.nwindDataSet
-			' 
-			' productsTableAdapter
-			' 
-			Me.productsTableAdapter.ClearBeforeFill = True
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(640, 456)
-			Me.Controls.Add(Me.chartControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            Me.nwindDataSet = New WindowsApplication1.nwindDataSet()
+            Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.productsTableAdapter = New WindowsApplication1.nwindDataSetTableAdapters.ProductsTableAdapter()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.productsBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.DataAdapter = Me.productsTableAdapter
+            Me.chartControl1.DataSource = Me.productsBindingSource
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            Me.chartControl1.Diagram = xyDiagram1
+            Me.chartControl1.Location = New System.Drawing.Point(24, 16)
+            Me.chartControl1.Name = "chartControl1"
+            Me.chartControl1.SeriesDataMember = "SupplierID"
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
+            Me.chartControl1.SeriesTemplate.ArgumentDataMember = "CategoryID"
+            Me.chartControl1.SeriesTemplate.SummaryFunction = "SUM([UnitsInStock])"
+            Me.chartControl1.Size = New System.Drawing.Size(592, 432)
+            Me.chartControl1.TabIndex = 0
+            AddHandler Me.chartControl1.BoundDataChanged, New DevExpress.XtraCharts.BoundDataChangedEventHandler(AddressOf Me.chartControl1_BoundDataChanged)
+            ' 
+            ' nwindDataSet
+            ' 
+            Me.nwindDataSet.DataSetName = "nwindDataSet"
+            Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' productsBindingSource
+            ' 
+            Me.productsBindingSource.DataMember = "Products"
+            Me.productsBindingSource.DataSource = Me.nwindDataSet
+            ' 
+            ' productsTableAdapter
+            ' 
+            Me.productsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(640, 456)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.productsBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
-		Private productsTableAdapter As WindowsApplication1.nwindDataSetTableAdapters.ProductsTableAdapter
-		Private nwindDataSet As nwindDataSet
-		Private productsBindingSource As System.Windows.Forms.BindingSource
-	End Class
+#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
+
+        Private productsTableAdapter As WindowsApplication1.nwindDataSetTableAdapters.ProductsTableAdapter
+
+        Private nwindDataSet As WindowsApplication1.nwindDataSet
+
+        Private productsBindingSource As System.Windows.Forms.BindingSource
+    End Class
 End Namespace
-
--- VB/WindowsApplication1/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,34 +1,28 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
-			Me.productsTableAdapter.Fill(Me.nwindDataSet.Products)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
+            productsTableAdapter.Fill(nwindDataSet.Products)
+        End Sub
 
-		Private Sub chartControl1_BoundDataChanged(ByVal sender As Object, ByVal e As EventArgs) Handles chartControl1.BoundDataChanged
-			Dim cc As ChartControl = TryCast(sender, ChartControl)
-			For Each s As Series In cc.Series
-				If Convert.ToInt32(s.Name) > 5 Then
-					s.Visible = False
-				End If
-
-			Next s
-		End Sub
-	End Class
+        Private Sub chartControl1_BoundDataChanged(ByVal sender As Object, ByVal e As EventArgs)
+            Dim cc As ChartControl = TryCast(sender, ChartControl)
+            For Each s As Series In cc.Series
+                If Convert.ToInt32(s.Name) > 5 Then s.Visible = False
+            Next
+        End Sub
+    End Class
 End Namespace
